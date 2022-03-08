04ea0a0279bdb7791382fa4a8207f0e2e5f63667
Mauricio Martinello <mauricio.martinello@compasso.com.br>
2022-03-08 08:54:24 -0300

    MovieCard

---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,19 @@
+import MovieCard from "components/MovieCard/MovieCard";
+import Pagination from "components/Pagination/Pagination";
+
+function Listing() {
+    return (
+        <>
+            <Pagination/>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                        <MovieCard/>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Listing;
