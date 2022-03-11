d0d65315d66a23c8882bd6d91d1f9bc400ce5a70
Mauricio Martinello <mauricio.martinello@compasso.com.br>
2022-03-10 22:33:19 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/mauricio/dsmovie/entities/Score.java
@@ -0,0 +1,29 @@
+package com.mauricio.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "tb_score")
+public class Score {
+
+	@EmbeddedId
+	private ScorePK id = new ScorePK();
+	private Double value;
+	
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+}
